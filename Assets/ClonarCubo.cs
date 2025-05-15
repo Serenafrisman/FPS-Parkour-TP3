84a9fe9edc4cd225fcdafc05f673549a0e8e60c1
Serenafrisman <49008981@est.ort.edu.ar>
2025-05-15 20:09:30 -0300

    .

---
+++ Assets/ClonarCubo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonarCubo : MonoBehaviour
+{
+    public GameObject CuboPrefab;
+
+    void Start()
+    {
+        Clonar(); // Llamás al método con el nuevo nombre
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Clonar() // Cambiaste el nombre del método
+    {
+        Instantiate(CuboPrefab);
+    }
+}
---
+++ Assets/Scripts/Clonador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clonador : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
